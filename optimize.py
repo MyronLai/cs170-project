d406bad0df406615764351ae4aefee2bf020479b
Aaron Opell <aaron.opell@live.com>
2020-04-21 21:36:06 -0700

    Added check_fn

--- optimize.py
+++ optimize.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 import scipy
 
+
 def campo_mcrt(graph):
     N = sizeof(graph)
     d = [0] * N
@@ -18,15 +19,27 @@ def campo_mcrt(graph):
         m[i] = max(m[i], weight)
         m[j] = max(m[j], weight)
         sumWeights += weight
-    mean = sumWeights/len(edges)
- 
+    mean = sumWeights / len(edges)
+
+
 def genetic_mcrt(graph):
     pass
 
-def check_fn(nodes):
-    return True # TODO: if valid placement
+
+def create_check_fn(graph):
+    def check_fn(nodes):
+        tree = [i for i in range(len(nodes)) if nodes[i]]
+        touched = set(tree)
+        for v in tree:
+            s = set([e[0] for e in graph[v]])
+            touched.update(s)
+        return len(touched) == len(nodes)
+    return check_fn
+
+
 
 def cost_fn(nodes):
-    return cost(approx_mcrt(nodes))  
+    return cost(approx_mcrt(nodes))
+
 
 scipy.optimize.basinhopping(cost_fn, initial_placement, accept_test=check_fn)
