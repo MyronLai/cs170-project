Fixed cost function normalization, shrink mat, connected check

utils.py
@@ -72,6 +72,9 @@ def verify_in_out(G, outfile):
         sp = line.split(" ")
         v1, v2 = int(sp[0]), int(sp[1])
         g.add_edge(v1, v2)
+    if not nx.is_connected(g):
+        print("Not connected!")
+        return False
     if not nx.is_tree(g):
         print("Not tree!")
         return False
@@ -91,8 +94,8 @@ def verify_in_out(G, outfile):
 # numba does not like
 def shrink_mat(G):
     """ Returns a new adjacency matrix with all the zero rows/columns (disconnected nodes) removed """
-    Grem = G[~np.all(G == 0, axis=0)]
-    Grem = Grem[~np.all(Grem == 0, axis=1)]
+    Grem = G[~np.all(G == 0, axis=1)]
+    Grem = Grem[:, ~np.all(Grem == 0, axis=0)]
     return Grem
 
 def mat_to_nx(G):
@@ -148,59 +151,4 @@ def cost_fn(G):
             min_count = min(counts[v], counts[u])
             # Don't multiply by 2 since each edge is counted twice anyways
             cost += min_count * (num_nodes - min_count) * G[u][v]
-    return cost
-
-def cost_fast(T):
-    """Calculates the average pairwise distance for a tree in linear time.
-    Since there is always unique path between nodes in a tree, each edge in the
-    tree is used in all of the paths from the connected component on one side
-    of the tree to the other. So each edge contributes to the total pairwise cost
-    in the following way: if the size of the connected components that are
-    created from removing an edge e are A and B, then the total pairwise distance
-    cost for an edge is 2 * A * B * w(e) = (# of paths that use that edge) * w(e).
-    We multiply by two to consider both directions that paths can take on an
-    undirected edge.
-    Since each edge connects a subtree to the rest of a tree, we can run DFS
-    to compute the sizes of all of the subtrees, and iterate through all the edges
-    and sum the pairwise distance costs for each edge and divide by the total
-    number of pairs.
-    This is very similar to Q7 on MT1.
-    h/t to Noah Kingdon for the algorithm.
-    """
-    if len(T) <= 1: return 0
-
-    if not nx.is_connected(T):
-        raise ValueError("Tree must be connected")
-
-    subtree_sizes = {}
-    marked = defaultdict(bool)
-    # store child parent relationships for each edge, because the components
-    # created when removing an edge are the child subtree and the rest of the vertices
-    root = list(T.nodes)[0];
-    
-    child_parent_pairs = [(root, root)]
-
-    def calculate_subtree_sizes(u):
-        """Iterates through the tree to compute all subtree sizes in linear time
-        Args:
-            u: the root of the subtree to start the DFS
-        """
-        unmarked_neighbors = filter(lambda v: not marked[v], T.neighbors(u))
-        marked[u] = True
-        size = 0
-        for v in unmarked_neighbors:
-            child_parent_pairs.append((v, u))
-            calculate_subtree_sizes(v)
-            size += subtree_sizes[v]
-        subtree_sizes[u] = size + 1
-        return subtree_sizes[u]
-
-    calculate_subtree_sizes(root)  # any vertex can be the root of a tree
-
-    cost = 0
-    for c, p in child_parent_pairs:
-        if c != p:
-            a, b = subtree_sizes[c], len(T.nodes) - subtree_sizes[c]
-            w = T[c][p]["weight"]
-            cost += 2 * a * b * w
-    return cost / (len(T) * (len(T) - 1))
+    return cost / (num_nodes * (num_nodes - 1))