ab3fec593662f51e1d4dd5799c3c945ca630e53d
Aaron Opell <aaron.opell@live.com>
2020-04-24 21:34:55 -0700

    Changed cost functions

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ def runfile(infile, outfile, score_to_beat = 1e99):
     G = utils.read_input(infile)
     print("Processing", infile)
     result, score = annealing.anneal(G, 120000, 0.2, 0.6, 0.0004, print_energy=False)
-    n = utils.shrink_mat(result).shape[0]
-    s = score / ((n * (n - 1)) / 2)
+    s = utils.cost_fast(utils.mat_to_nx(utils.shrink_mat(result)))
+
     print(f"- {s}")
     if s < score_to_beat:
         utils.write_output(result, G, outfile)
