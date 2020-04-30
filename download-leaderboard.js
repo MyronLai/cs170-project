00a19920c3ee876317a442d114c4b3048e4230d7
Aaron Opell <aaron.opell@live.com>
2020-04-30 14:23:37 -0700

    updated leaderboard scraping script

--- download-leaderboard.js
+++ download-leaderboard.js
@@ -12,17 +12,20 @@ File should begin with the following:
             ...
 */
 
-let messages = "";
+let messages = [];
 let a = `{"t":"d","d":{"r":1,"a":"s","b":{"c":{"sdk.js.7-14-0":1}}}}`;
 let b = `{"t":"d","d":{"r":2,"a":"q","b":{"p":"/leaderboard","q":{"sp":"small-1","ep":"small-1","i":"input"},"t":1,"h":""}}}`;
 var ws = new WebSocket("wss://s-usc1c-nss-233.firebaseio.com/.ws?v=5&ns=cs-170-project-sp20");
 let sent = false;
 ws.onmessage = function(event) {
-   messages += event.data
+   messages.push(event.data);
    if(!sent) {
         ws.send(a);
         ws.send(b);
         sent = true;
    }
 }
-copy(messages)
+
+// After a couple seconds run
+json = messages.slice(3,-1).join("")
+copy(json)
